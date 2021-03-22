4cc45a504c68cfe61dbdb91dee51b537a95a44b3
Hiranandani, Himanshu <hhiranandani@DELOITTE.com>
2021-03-22 18:33:31 +0530

    Adds dockerfile and dockercompose

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -6,7 +6,10 @@ export const databaseProviders = [
     useFactory: async () =>
       await createConnection({
         type: 'postgres',
-        url: process.env.DATABASE_URL,
+        // host: process.env.DB_HOST,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE_NAME,
         entities: [path.join(__dirname, '../**/**/**/*.entity{.ts,.js}')],
         synchronize: process.env.NODE_ENV === 'local',
         extra: {
